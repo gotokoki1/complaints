503246673a064717cd198eb9483dfe647b8c9072
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2021-03-17 11:42:50 +0900

    投稿表示数変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
  
         return view('post.index', [
             'posts' => $posts,
